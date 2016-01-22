5e9d6dfc07b4ac18c5778b62ae3e0f2812d42473
tresspass <tresspass5299@gmail.com>
2016-01-22 10:07:44 -0500

    Update Saver.java

--- Cross Country Coach Simulator/workspace/Coach Simulator/src/coachsimulator/Saver.java
+++ Cross Country Coach Simulator/workspace/Coach Simulator/src/coachsimulator/Saver.java
@@ -1,9 +1,30 @@
 public class Saver(){
   String name;
-  public Saver(String name){
+  File file;
+  File fileDir;
+  Path path;
+  public Saver(String name, String data){
     this.name = name;
+    path = System.getenv("APPDATA");
   }
   public void Save(){
-  
+    fileDir = new File(path);
+    if(!file.exists){
+      createDir();
+    }else if(file.exists){
+      if(file.exists){
+        
+      }
+    }else{
+      return;
+    }
   }
+  public void createDir(){
+    try{
+      new File(path).mkdir();
+    }catch(){
+      throw new Error("Error creating directory. Path: "+ path + " not found");
+    }
+      
+    }
 }
