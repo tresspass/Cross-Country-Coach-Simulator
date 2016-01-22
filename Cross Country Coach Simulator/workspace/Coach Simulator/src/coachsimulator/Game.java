ae649190c0a62ba4451663d28deeb861e67d77f0
tresspass <tresspass5299@gmail.com>
2016-01-21 23:49:51 -0500

    y
    cool

---
+++ Cross Country Coach Simulator/workspace/Coach Simulator/src/coachsimulator/Game.java
@@ -0,0 +1,19 @@
+package coachsimulator;
+
+import javax.swing.JPanel;
+
+//Main game organizer
+public class Game extends JPanel{
+	
+	public static void init() {
+		
+	}
+	
+	public static void update() {
+		System.out.println("foo");
+	}
+
+	public static void render() {
+		
+	}
+}
