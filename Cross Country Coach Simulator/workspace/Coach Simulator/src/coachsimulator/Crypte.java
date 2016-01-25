c43d851142b362ef06cfb2572cd500765def5650
tresspass <tresspass5299@gmail.com>
2016-01-25 10:06:32 -0500

    Update and rename Encrypte.java to Crypte.java

---
+++ Cross Country Coach Simulator/workspace/Coach Simulator/src/coachsimulator/Crypte.java
@@ -0,0 +1,15 @@
+public class Crypt{
+  public void Crypt(int type, File file){
+    if(type == 0){
+      encrypt();
+    }else if(type == 1){
+      decrypt();
+    }
+  }
+  public static void encrypt(){
+    
+  }
+  public static void decrypt(){
+    
+  }
+}
